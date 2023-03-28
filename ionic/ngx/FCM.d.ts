26932cc706de7ebdf4795216ee1d1dbe37aa8524
Brice <bpowell@healthrecoverysolutions.com>
2023-03-28 12:40:04 -0400

    Add fucntion to type file

--- ionic/ngx/FCM.d.ts
+++ ionic/ngx/FCM.d.ts
@@ -116,4 +116,17 @@ export declare class FCM {
      * @returns {Promise<void>} Async call to native implementation
      */
     unsubscribeFromTopic(topic: string): Promise<void>;
+
+    initDifferentAccount(accountInfo: AccountInfo): Promise<void>
+}
+
+interface AccountInfo {
+    project_number: string;
+    database_url: string;
+    project_id: string;
+    storage_bucket: string;
+    app_id: string;
+    api_key: string;
+    package_name: string;
+    client_id: string;
 }
